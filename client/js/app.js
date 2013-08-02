1163c78295a17a23eb6f3bf2caa6ef3d68758eb8
David Higginbotham <davehigginbotham@gmail.com>
2013-08-02 17:21:42 -0600

    boned

--- client/js/app.js
+++ client/js/app.js
@@ -1,14 +1,1 @@
 'use strict';
-
-// Models
-window.Models = Backbone.Model.extend();
-
-window.ModelsCollection = Backbone.Collection.extend({
-    model:Models
-});
-
-window.Model = new ModelsCollection({
-  base: "/api/apps" 
-});
-
-Backbone.history.start();
